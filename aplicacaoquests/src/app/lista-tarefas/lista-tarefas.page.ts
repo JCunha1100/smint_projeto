10a880ce4e7154abe95bdfd86bedc70b1fb83767
SouVlad <vladafife@gmail.com>
2026-01-05 22:50:21 +0000

    Adicionar projeto completo aplicacaoquests - Ionic/Angular

---
+++ aplicacaoquests/src/app/lista-tarefas/lista-tarefas.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-tarefas',
+  templateUrl: './lista-tarefas.page.html',
+  styleUrls: ['./lista-tarefas.page.scss'],
+  standalone: false
+})
+export class ListaTarefasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ aplicacaoquests/src/app/register-login/register-login.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-login',
+  templateUrl: './register-login.page.html',
+  styleUrls: ['./register-login.page.scss'],
+  standalone: false
+})
+export class RegisterLoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
