3aede3814f859fe3bcaab8576f9758479a43567b
SouVlad <vladafife@gmail.com>
2026-01-06 10:08:43 +0000

    atualização-register-login

--- src/app/register-login/register-login.page.ts
+++ src/app/register-login/register-login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-login',
@@ -8,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterLoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  goToRegister() {
+    this.router.navigate(['/registar']);
   }
 
 }
